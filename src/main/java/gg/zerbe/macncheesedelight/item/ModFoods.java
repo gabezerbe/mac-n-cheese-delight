c20d7804b5d2437d28a094f2e1062e55e3feabb4
Gabe <gabe.zerbe@gmail.com>
2024-07-27 15:59:51 -0400

    Making everything so far a food item

---
+++ src/main/java/gg/zerbe/macncheesedelight/item/ModFoods.java
@@ -0,0 +1,22 @@
+package gg.zerbe.macncheesedelight.item;
+
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
+
+public class ModFoods {
+
+    public static final FoodProperties CHEDDAR_CHEESE = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationMod(0.2f)
+            .build();
+
+    public static final FoodProperties MOZZARELLA_CHEESE = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationMod(0.2f)
+            .build();
+
+    public static final FoodProperties BOWL_OF_MAC_N_CHEESE = new FoodProperties.Builder()
+            .nutrition(12)
+            .saturationMod(2.4f)
+            .build();
+}
--- src/main/java/gg/zerbe/macncheesedelight/item/ModItems.java
+++ src/main/java/gg/zerbe/macncheesedelight/item/ModItems.java
@@ -12,14 +12,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MacNCheeseDelightMod.MOD_ID);
 
-
     public static final RegistryObject<Item> CHEDDAR_CHEESE = ITEMS.register("cheddar_cheese",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoods.CHEDDAR_CHEESE)));
 
     public static final RegistryObject<Item> MOZZARELLA_CHEESE = ITEMS.register("mozzarella_cheese",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoods.MOZZARELLA_CHEESE)));
     public static final RegistryObject<Item> BOWL_OF_MAC_AND_CHEESE = ITEMS.register("bowl_of_mac_n_cheese",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoods.BOWL_OF_MAC_N_CHEESE)));
 
     public static void register(IEventBus bus){
         ITEMS.register(bus);
